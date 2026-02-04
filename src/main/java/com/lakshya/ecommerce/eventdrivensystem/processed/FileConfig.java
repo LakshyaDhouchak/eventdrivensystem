e55d75f0409c6f7c6bbe45d73fd98c0aac4c5024
lakshya <lakshya10171@gmail.com>
2026-02-04 22:48:52 +0530

    add the fileConfig class

---
+++ src/main/java/com/lakshya/ecommerce/eventdrivensystem/processed/FileConfig.java
@@ -0,0 +1,16 @@
+package com.lakshya.ecommerce.eventdrivensystem.processed;
+
+import java.io.File;
+
+public class FileConfig {
+    // define the properties
+    public static final String INPUT_DIR = "input";
+    public static final String OUTPUT_DIR = "output";
+    public static final String INVENTORY_FILE = INPUT_DIR + File.separator + "ecommerce_datasets.csv";
+    public static final String ORDERS_FILE = INPUT_DIR + File.separator + "products.csv";
+    public static final String UPDATED_ORDERS = OUTPUT_DIR + File.separator +"ecommerce_final_datasets.csv";
+
+    // define the header attribute fo find
+    public static final String COL_PRODUCT_ID = "product_id";
+    public static final String COL_CATAGORY = "category";
+}
