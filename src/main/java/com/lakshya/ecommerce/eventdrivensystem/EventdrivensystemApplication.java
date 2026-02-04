812814bbffe02739d56f0f013c1ed7eeff1b4e1b
lakshya <lakshya10171@gmail.com>
2026-02-04 11:02:53 +0530

    first commit

---
+++ src/main/java/com/lakshya/ecommerce/eventdrivensystem/EventdrivensystemApplication.java
@@ -0,0 +1,13 @@
+package com.lakshya.ecommerce.eventdrivensystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventdrivensystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventdrivensystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lakshya/ecommerce/eventdrivensystem/EventdrivensystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshya.ecommerce.eventdrivensystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventdrivensystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
