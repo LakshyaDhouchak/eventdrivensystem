a445b48b91142a29c28c69eb70cb91fd0c0252eb
lakshya <lakshya10171@gmail.com>
2026-02-06 20:57:42 +0530

    add inventory staging repository

--- src/main/java/com/lakshya/ecommerce/eventdrivensystem/repository/InventoryStagingRepository.java
+++ src/main/java/com/lakshya/ecommerce/eventdrivensystem/repository/InventoryStagingRepository.java
@@ -1,5 +1,8 @@
 package com.lakshya.ecommerce.eventdrivensystem.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,9 @@ import com.lakshya.ecommerce.eventdrivensystem.entity.InventoryStaging;
 
 @Repository
 public interface InventoryStagingRepository extends JpaRepository<InventoryStaging , Long> {
+
+    void saveAll(List<InventoryStaging> batch);
+
+    void deleteByPriceLessThanEqual(BigDecimal zero);
     
 }
