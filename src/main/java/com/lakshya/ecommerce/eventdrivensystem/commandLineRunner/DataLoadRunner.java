8537b8ca71b109bb1b98c40c6c7d8f3f4df35805
lakshya <lakshya10171@gmail.com>
2026-02-06 23:05:04 +0530

    data loadRunner

---
+++ src/main/java/com/lakshya/ecommerce/eventdrivensystem/commandLineRunner/DataLoadRunner.java
@@ -0,0 +1,21 @@
+package com.lakshya.ecommerce.eventdrivensystem.commandLineRunner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.lakshya.ecommerce.eventdrivensystem.processed.FileConfig;
+import com.lakshya.ecommerce.eventdrivensystem.service.InventoryServiceInterface;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoadRunner implements CommandLineRunner{
+    // define the properties
+    private InventoryServiceInterface inventoryServiceInterface;
+    @Override
+    public void run(String... args) throws Exception {
+        inventoryServiceInterface.ingectCsv(FileConfig.INVENTORY_FILE);
+    }
+    
+}
