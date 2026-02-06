30aea55db7c340132bef4660e755fed21bde3d2a
lakshya <lakshya10171@gmail.com>
2026-02-06 23:14:16 +0530

    update in staging repo

--- src/main/java/com/lakshya/ecommerce/eventdrivensystem/repository/InventoryStagingRepository.java
+++ src/main/java/com/lakshya/ecommerce/eventdrivensystem/repository/InventoryStagingRepository.java
@@ -1,7 +1,7 @@
 package com.lakshya.ecommerce.eventdrivensystem.repository;
 
 import java.math.BigDecimal;
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
