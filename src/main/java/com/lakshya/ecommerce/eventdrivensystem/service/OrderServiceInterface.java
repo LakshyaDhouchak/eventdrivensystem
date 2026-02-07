9aca0186d3f99e4e3948b6eef8312d27f7927af4
lakshya <lakshya10171@gmail.com>
2026-02-07 21:19:22 +0530

    implement the order ingestion Service

---
+++ src/main/java/com/lakshya/ecommerce/eventdrivensystem/service/OrderServiceInterface.java
@@ -0,0 +1,9 @@
+package com.lakshya.ecommerce.eventdrivensystem.service;
+
+import com.lakshya.ecommerce.eventdrivensystem.dto.OrderCustomerProductReviewParsedDTO;
+
+public interface OrderServiceInterface {
+    // define the methord
+    void ingestOrder(OrderCustomerProductReviewParsedDTO dto);
+    void simulate(String filePath);
+}
