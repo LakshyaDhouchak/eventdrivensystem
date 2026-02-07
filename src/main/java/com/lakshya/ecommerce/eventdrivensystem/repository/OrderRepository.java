d9ee6b238693695bc0b3210b240e73099d52f64a
lakshya <lakshya10171@gmail.com>
2026-02-07 15:49:00 +0530

    implement the order Repository

---
+++ src/main/java/com/lakshya/ecommerce/eventdrivensystem/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.lakshya.ecommerce.eventdrivensystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lakshya.ecommerce.eventdrivensystem.entity.Orders;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Orders , Long> {
+
+    
+} 
