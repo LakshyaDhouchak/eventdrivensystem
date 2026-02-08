3d53b5d2dc3676633e8c4829cf2e8088488031bb
lakshya <lakshya10171@gmail.com>
2026-02-08 09:20:49 +0530

    i remmoved it and implement microservice architecture

--- src/main/resources/db/migration/V7__products_table.sql
+++
@@ -1,8 +0,0 @@
--- make the product table
-CREATE TABLE IF NOT EXISTS products(
-    product_id BIGINT PRIMARY KEY,
-    product_name VARCHAR(255),
-    category VARCHAR(100),
-    unit_price DECIMAL(10,2),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
